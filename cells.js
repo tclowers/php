bb5113e46daca390084883a7c69fdf87e84ec6eb
tom <tclowers@gmail.com>
2014-03-25 23:30:59 -0700

    small cosmetic changes to html and css

--- cells.js
+++ cells.js
@@ -5,55 +5,35 @@ var toType = function(obj) {
 }
 
 var get_cells = function() {
-	//$('.generations').fadeOut(3000, function(){
-		$.ajax({
-			url: 'cells2.php',
-			type: 'GET',
-			dataType: 'json',
-			success: function(result) {
-				var content = '';
-				$.each(result, function(index, row) {
-						for(var a = 0; a < row.length; a++) {
-							//content += "type: " + toType(row[a]) + " row: " + row[a] + ", ";
-							if (row[a]) {
-								content += "<i class='fa fa-circle'></i>";
-							} else {
-								content += "<i class='fa fa-circle-o'></i>";
-							}
-							/*if(element == 'true') {
-								content += "<i class='fa fa-circle'></i>";
-							} else {
-								content += "false";
-								//content += "<i class='fa fa-circle-o'></i>";
-							}*/
-						}
-						//content += "index: " + index + " row isa: " + toType(row) + " row: " + row;
-					/*$.each(row, function(element) {
-						//content += element;
-						//content += row;
-						/*if(element == true) {
-							content += "<i class='fa fa-circle'></i>";
+	$.ajax({
+		url: 'Automata.php',
+		type: 'GET',
+		dataType: 'json',
+		success: function(result) {
+			var content = '';
+			$.each(result, function(index, row) {
+					for(var a = 0; a < row.length; a++) {
+						//content += "type: " + toType(row[a]) + " row: " + row[a] + ", ";
+						if (row[a]) {
+							content += "<i class='fa fa-circle cell'></i>";
 						} else {
-							content += "false";
-							//content += "<i class='fa fa-circle-o'></i>";
-						}*/
-					//});
-					content += "<br/>";
-				});
-				//$('.generations').html(content).fadeIn(3000, get_cells());
-				$('.generations').html(content);
-				if(running) {
-					get_cells(running);					
-				}
-
-				
-			},
-			error: function(request, errorType, errorMessage) {
-				console.log('Error: ' + errorType + ' with message: ' + errorMessage);
+							content += "<i class='fa fa-circle-o cell'></i>";
+						}
+					}
+				content += "<br/>";
+			});
+			//$('.generations').html(content).fadeIn(3000, get_cells());
+			$('.generations').html(content);
+			if(running) {
+				get_cells(running);					
 			}
-		});
-	//});
-	
+
+			
+		},
+		error: function(request, errorType, errorMessage) {
+			console.log('Error: ' + errorType + ' with message: ' + errorMessage);
+		}
+	});	
 }
 
 $(document).ready(function(){
